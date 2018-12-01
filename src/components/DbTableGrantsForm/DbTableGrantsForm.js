7a3c24e6a0a18e4f5e0a21f53437a13b25dc963d
Alexander Savin <box.for.postman@gmail.com>
2018-12-01 18:44:52 +0300

    DbTableGrantsForm styles

--- src/components/DbTableGrantsForm/DbTableGrantsForm.js
+++ src/components/DbTableGrantsForm/DbTableGrantsForm.js
@@ -4,6 +4,9 @@ import { observer } from 'mobx-react';
 import { Column, Table, AutoSizer } from 'react-virtualized';
 import withStyles from '@material-ui/core/styles/withStyles';
 import Typography from '@material-ui/core/Typography';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Tooltip from '@material-ui/core/Tooltip';
 import Checkbox from '@material-ui/core/Checkbox';
 import Divider from '@material-ui/core/Divider';
 import TableCell from '@material-ui/core/TableCell';
@@ -12,12 +15,39 @@ import { dbColumnPrivilegeTypes } from '../../models/DbColumn';
 import { dbTablePrivilegeTypes } from '../../models/DbTable';
 import Progress from '../Progress';
 
-const styles = () => ({
+const styles = (theme) => ({
   progress: {
     display: 'flex',
     alignItems: 'center',
     flexDirection: 'column',
-  }
+  },
+  tableGrantsWrapper: {
+    display: 'flex',
+    alignItems: 'center',
+    marginBottom: theme.spacing.unit * 2,
+  },
+  tableGrantsLabel: {
+    paddingRight: theme.spacing.unit,
+  },
+  headerCell: {
+    flexGrow: 1,
+    display: 'flex',
+    overflow: 'hidden',
+    alignItems: 'center',
+  },
+  headerText: {
+    maxWidth: '100%',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+  },
+  columnCell: {
+    display: 'flex',
+    alignItems: 'center',
+    flexGrow: 1
+  },
+  checkboxCell: {
+    flexGrow: 1,
+  },
 });
 
 type Props = {
@@ -25,45 +55,55 @@ type Props = {
   dbConnectionsManager: DbConnectionsManager,
 };
 
-function headerRenderer({ dataKey, label }) {
-  return (
-    <TableCell component="div" variant="head" padding="checkbox" style={{
-      flexGrow: 1,
-      overflow: 'hidden',
-      textOverflow: 'ellipsis',
-      display: 'flex',
-      alignItems: 'center',
-    }}>
-      <Typography variant="subtitle2">{label}</Typography>
-    </TableCell>
-  );
-}
-
-const checkboxRenderer = ({ cellData }) => (
-  <TableCell component="div" variant="body" padding="checkbox" style={{ flexGrow: 1 }}>
-    <Checkbox checked={cellData}/>
-  </TableCell>
-);
-const columnRenderer = ({ cellData }) => (
-  <TableCell component="div" variant="body" padding="checkbox" style={{ display: 'flex', alignItems: 'center', flexGrow: 1 }}>
-    <Typography variant="body1" style={{ flexGrow: 1 }}>{cellData}</Typography>
-  </TableCell>
-);
+const rowStyle= { display: 'flex' };
+const columnStyle = { display: 'flex' };
+const headerStyle = { display: 'flex', minWidth: '0px' };
 
 class DbTableGrantsForm extends React.Component<Props> {
+
+  headerRenderer = ({ dataKey, label }) => {
+    const { classes } = this.props;
+    return (
+      <TableCell component="div" variant="head" padding="checkbox" className={classes.headerCell}>
+        <Tooltip title={label}>
+          <Typography variant="h6" className={classes.headerText}>
+            {label}
+          </Typography>
+        </Tooltip>
+      </TableCell>
+    );
+  };
+
+  columnRenderer = ({ cellData }) => {
+    const { classes } = this.props;
+    return (
+      <TableCell component="div" variant="body" padding="checkbox" className={classes.columnCell}>
+        <Typography variant="body1" style={{ flexGrow: 1 }}>{cellData}</Typography>
+      </TableCell>
+    );
+  };
+
+  checkboxRenderer = ({ cellData }) => {
+    const { classes } = this.props;
+    return (
+      <TableCell component="div" variant="body" padding="checkbox" className={classes.checkboxCell}>
+        <Checkbox checked={cellData}/>
+      </TableCell>
+    );
+  };
 
   renderNoRows = () => {
     const { dbConnectionsManager, classes } = this.props;
     const currentTable = dbConnectionsManager.getCurrentTable();
     if (!currentTable.privilegesFetcher.inSuccessState || !currentTable.columnsFetcher.inSuccessState) {
-      return <div className={classes.progress}><Progress/></div>
+      return <div className={classes.progress}><Progress/></div>;
     }
 
     return <Typography variant="h6" align="center">No columns found</Typography>;
   };
 
   render() {
-    const { dbConnectionsManager } = this.props;
+    const { dbConnectionsManager, classes } = this.props;
     const currentTable = dbConnectionsManager.getCurrentTable();
     const grantee = dbConnectionsManager.currentRoleName;
     const grantedData = currentTable ? currentTable.columnsNames.map(columnName => {
@@ -81,33 +121,37 @@ class DbTableGrantsForm extends React.Component<Props> {
     }) : [];
 
     const heightOfCell = 49;
+    const headerHeight = 56;
     const totalHeight = grantedData.length * heightOfCell;
-    const height = (totalHeight > 500 ? 500 : totalHeight) || 142;
+    const height = ((totalHeight > 500 ? 500 : totalHeight) || 86) + headerHeight;
 
     return (
       <React.Fragment>
-        <div><Typography variant="h6">
-          {dbTablePrivilegeTypes.map(type => {
-            return (
-              <React.Fragment key={type}>
-                <Checkbox checked={!!(currentTable && currentTable.privileges.get(grantee, type))}/>&nbsp;{
-                  capitalize(type)
-                }
-              </React.Fragment>
-            );
-          })}</Typography>
+        <div className={classes.tableGrantsWrapper}>
+          <Typography variant="h6" className={classes.tableGrantsLabel}>Table:</Typography>
+          <FormGroup row>
+            {dbTablePrivilegeTypes.map(type => {
+              return (
+                <FormControlLabel
+                  key={type}
+                  control={<Checkbox checked={!!(currentTable && currentTable.privileges.get(grantee, type))}/>}
+                  label={capitalize(type)}
+                />
+              );
+            })}
+          </FormGroup>
         </div>
-        <Divider />
+        <Divider/>
         <AutoSizer disableHeight>
           {({ width }) => (
             <Table
               width={width}
               height={height}
-              headerHeight={56}
+              headerHeight={headerHeight}
               rowHeight={heightOfCell}
               rowCount={grantedData.length}
               rowGetter={({ index }) => grantedData[index]}
-              rowStyle={{ display: 'flex' }}
+              rowStyle={rowStyle}
               noRowsRenderer={this.renderNoRows}
             >
               <Column
@@ -115,11 +159,11 @@ class DbTableGrantsForm extends React.Component<Props> {
                 dataKey="name"
                 label="Column"
                 width={100}
-                flexGrow={1}
-                headerRenderer={headerRenderer}
-                cellRenderer={columnRenderer}
-                style={{ display: 'flex' }}
-                headerStyle={{ display: 'flex', minWidth: '0px' }}
+                flexGrow={2}
+                headerRenderer={this.headerRenderer}
+                cellRenderer={this.columnRenderer}
+                style={columnStyle}
+                headerStyle={headerStyle}
               />
               {dbColumnPrivilegeTypes.map(type => (
                 <Column
@@ -127,10 +171,11 @@ class DbTableGrantsForm extends React.Component<Props> {
                   dataKey={type}
                   label={capitalize(type)}
                   width={100}
-                  headerRenderer={headerRenderer}
-                  cellRenderer={checkboxRenderer}
-                  style={{ display: 'flex' }}
-                  headerStyle={{ display: 'flex', minWidth: '0px' }}
+                  flexGrow={1}
+                  headerRenderer={this.headerRenderer}
+                  cellRenderer={this.checkboxRenderer}
+                  style={columnStyle}
+                  headerStyle={headerStyle}
                 />
               ))}
             </Table>
